293795234d47c30932536fbd72945320d3eb852c
naolumer <naolumer3@gmail.com>
2024-09-22 12:17:16 +0300

    Intro to React

---
+++ REACT/my-first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./assets/Button.jsx"
+import MyComponent from "./assets/MyComponenet.jsx"
+import Counter from "./assets/Counter.jsx"
+
+function App() {
+  return (
+    <Counter />
+  );
+}
+ 
+
+export default App
---
+++ REACT/my-first-react-app/src/assets/Button.jsx
@@ -0,0 +1,12 @@
+
+
+function Button(){
+    
+    const handleClick = (e) => e.target.textContent = "OUCH!";
+
+    return(<button onDoubleClick ={(e) => handleClick(e)}>Click me **</button>);
+}
+
+
+
+export default Button
