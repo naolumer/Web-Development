223a6ce60e2b231590e5865c446209fb38cb231a
naolumer <naolumer3@gmail.com>
2024-09-23 23:48:12 +0300

    to do list using useState

--- REACT/my-first-react-app/src/App.jsx
+++ REACT/my-first-react-app/src/App.jsx
@@ -2,10 +2,11 @@ import Button from "./assets/Button.jsx"
 import MyComponent from "./assets/MyComponenet.jsx"
 import Counter from "./assets/Counter.jsx"
 import UpdateArrays from "./assets/UpdateArrays.jsx";
+import ToDoList from "./assets/ToDoList.jsx";
 
 function App() {
   return (
-    <UpdateArrays />
+    <ToDoList />
   );
 }
  
