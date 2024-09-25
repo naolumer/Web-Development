3b2273d9b09476784aa539175912a346634f7cc8
naolumer <naolumer3@gmail.com>
2024-09-25 21:35:56 +0300

    Digital clock using useEffect hook

--- REACT/my-first-react-app/src/App.jsx
+++ REACT/my-first-react-app/src/App.jsx
@@ -3,10 +3,11 @@ import MyComponent from "./assets/MyComponenet.jsx"
 import Counter from "./assets/Counter.jsx"
 import UpdateArrays from "./assets/UpdateArrays.jsx";
 import ToDoList from "./assets/ToDoList.jsx";
+import DigitalClock from "./assets/DigitalClock.jsx";
 
 function App() {
   return (
-    <ToDoList />
+    <DigitalClock />
   );
 }
  
