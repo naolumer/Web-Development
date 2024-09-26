b645639bf0d7a939fd2cc509a382021d68dee7f8
naolumer <naolumer3@gmail.com>
2024-09-26 14:53:56 +0300

    Stopwatch app using useRef hook.

--- REACT/my-first-react-app/src/App.jsx
+++ REACT/my-first-react-app/src/App.jsx
@@ -4,10 +4,12 @@ import Counter from "./assets/Counter.jsx"
 import UpdateArrays from "./assets/UpdateArrays.jsx";
 import ToDoList from "./assets/ToDoList.jsx";
 import DigitalClock from "./assets/DigitalClock.jsx";
+import Stopwatch from "./assets/Stopwatch.jsx";
+
 
 function App() {
   return (
-    <DigitalClock />
+    <Stopwatch />
   );
 }
  
