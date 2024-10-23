54946f2b8b742f55bc8c53eb0487f9ef1ccb1822
naolumer <naolumer3@gmail.com>
2024-10-23 21:51:12 +0300

    Front end including login page

---
+++ REACT/Netflix-Clone/netflix/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from './Pages/Home/Home'
+import {Routes, Route} from 'react-router-dom'
+import Login from './Pages/Login/Login'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
+      
+
+    </div>
+  )
+}
+
+export default App
