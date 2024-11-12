f0d9f959d553159245f0273179476f9c9e7fd4e3
naolumer <naolumer3@gmail.com>
2024-11-12 22:04:16 +0300

    footer section

--- REACT/Real Estate Website/RealEstate/src/App.jsx
+++ REACT/Real Estate Website/RealEstate/src/App.jsx
@@ -4,15 +4,21 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className='w-full overflow-hidden'> 
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
--- REACT/Real Estate Website/RealEstate/src/components/Contact.jsx
+++ REACT/Real Estate Website/RealEstate/src/components/Contact.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import { toast } from 'react-toastify';
 
 function Contact() {
+
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "61ffaa54-605f-4fb3-b21c-2d667790c8d3");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message)
+      setResult('');
+    }
+  };
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden'
     id='Contact'>
@@ -10,7 +38,7 @@ function Contact() {
         <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? 
         Let's Build Your Future Together</p>
 
-        <form className='max-w-2xl mx-auto text-gray-600 pt-8'>
+        <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
             <div className='flex flex-wrap'>
                 <div className='w-full md:w-1/2 text-left'>
                     Your Name
@@ -25,6 +53,14 @@ function Contact() {
                     required />
                 </div>
             </div>
+            <div>
+                Message
+                <textarea className='w-full border border-gray-300 rounded py-3
+                px-4 mt-2 h-48 resize none' placeholder='Message' name="Message" required>
+                </textarea>
+            </div>
+            <button className='px-12 py-2 mx-auto text-center rounded md mt-8 bg-blue-700 text-white '>
+               {result? result:" Send Message"}</button>
 
         </form>
     </div>
