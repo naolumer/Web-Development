f8666eb6778e73e68e49d1af6f21ba4fa87187c9
naolumer <naolumer3@gmail.com>
2024-12-11 11:42:20 +0300

    folder setup for shoeFilterApp

---
+++ REACT/Shoe-Filtering/shoeFiltering/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './Navigation/Nav'
+import Recommended from './Recommended/Recommended'
+import Products from './Products/Products'
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Products/>
+      <Recommended/>
+
+    </div>
+  )
+}
+
+export default App
