47cf471d3b0dcca7e15573bcba7c72edc7cf2a72
naolumer <naolumer3@gmail.com>
2025-03-08 11:34:01 +0300

    apply (toggle) classList based on the outcome

---
+++ Javascript-course/Rock-Paper-Scissors/index.js
@@ -0,0 +1,50 @@
+
+const playerChoice = document.getElementById("playerchoice")
+const computerChoice = document.getElementById("computerchoice")
+const resultelement = document.getElementById("result")
+const playerScore = document.getElementById("playerscore")
+const computerScore = document.getElementById("computerscore")
+
+const randomList = ["rock","paper","scissors"]
+
+let playerscore = 0
+let computerscore = 0
+
+
+const playGame = (playerchoice)=>{
+    const randomIndex = Math.floor(Math.random()*3)
+    const compchoice = randomList[randomIndex]
+    computerChoice.textContent = compchoice
+    playerChoice.textContent = playerchoice
+
+    if (playerchoice===compchoice) {
+        resultelement.textContent = "IT'S A TIE!"
+        resultelement.classList.remove("redColor")
+        resultelement.classList.remove("greenColor")
+    } else {
+        let res;
+        switch(playerchoice) {
+            case "rock":
+                res=(compchoice==="scissors"? "YOU WIN !":"YOU LOSE" )
+                break
+            case "paper":
+                res=(compchoice==="rock"? "YOU WIN !":"YOU LOSE" )
+                break
+            case "scissors":
+                res=(compchoice==="paper"? "YOU WIN !":"YOU LOSE" )
+                break  
+        }
+        resultelement.textContent = res
+    }
+    if (resultelement.textContent==="YOU WIN !") {
+        playerscore++
+        playerScore.textContent = playerscore
+        resultelement.classList.remove("redColor","greenColor")
+        resultelement.classList.toggle("greenColor")
+    } else if (resultelement.textContent==="YOU LOSE"){
+        computerscore++
+        computerScore.textContent = computerscore
+        resultelement.classList.remove("redColor","greenColor")
+        resultelement.classList.toggle("redColor")
+    } 
+}
