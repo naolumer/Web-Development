78411d89247bfe44f22f30534f04b2dc62246f71
naolumer <naolumer3@gmail.com>
2025-04-03 16:07:53 +0300

    replaced logo with mine

--- REACT/youtubeClone/youtube/src/Components/Navbar/Navbar.jsx
+++ REACT/youtubeClone/youtube/src/Components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import more_icon from "../../assets/more.png"
 import notification_icon from "../../assets/notification.png"
 import profile_icon from "../../assets/user_profile.jpg"
 import { Link } from 'react-router-dom'
+import natube from "../../assets/natube.png"
 
 
 
@@ -16,7 +17,7 @@ function Navbar({setSidebar}) {
     <nav className='flex-div'>
         <div className='nav-left flex-div'>
             <img className='menu-icon' onClick={()=>setSidebar(prev=>prev===false?true: false)} src={menu_icon} alt="" />
-            <Link to='/'><img className='logo' src={logo} alt="" /></Link>
+            <Link to='/'><img className='logo' src={natube} alt="" /></Link>
         </div>
 
         <div className='nav-middle flex-div'>
